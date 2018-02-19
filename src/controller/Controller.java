422b1a8a713be98c0013c4b7c2aaf9cba0b208a3
Vernitrax <ty21@op.pl>
2018-02-19 10:34:39 +0100

    import added, constructor created for controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,10 +1,19 @@
 package library;
 
 import dao.DAO;
+import view.View;
 
 public class Controller {
 
-    public Controller{}
+    private DAO dao;
+    private View view;
+
+    public Controller{
+    
+        this.dao = new DAO();
+        this.view = new View();
+    
+    }
     
     public void start() {
         boolean isAppRunning = true;
