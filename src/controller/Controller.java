1b5ddc28060ba721ef2964c34ddd5b791455d02f
Vernitrax <ty21@op.pl>
2018-02-19 10:48:48 +0100

    start method implemented, all options method created, in controller

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -8,7 +8,7 @@ public class Controller {
     private DAO dao;
     private View view;
 
-    public Controller{
+    public Controller() {
     
         this.dao = new DAO();
         this.view = new View();
@@ -17,6 +17,43 @@ public class Controller {
     
     public void start() {
         boolean isAppRunning = true;
+        view.handleMenu();
+        while (isAppRunning) {
+            view.displayMenu();
+            String userInput = view.getStringInput();
+            switch (userInput) {
+                case "1":
+                    addNew();
+                    break;
+                case "2":
+                    Edit();
+                    break;
+                case "3":
+                    Delete();
+                    break;
+                case "4":
+                    Search();
+                    break;
+                case "5":
+                    SeeAll();
+                    break;
+                case "6":
+                    SeeByAuthor();
+                    break;
+                case "0":
+                    isAppRunning = false;
+                    break;
+                default:
+                    view.displayWrongInputMessage();
+            }
+        }
     }
+    
+    private void addNew() {}
+    private void Edit() {}
+    private void Delete() {}
+    private void Search() {}
+    private void SeeAll() {}
+    private void SeeByAuthor() {}
 
 }
