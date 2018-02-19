206633223ceffc78e860588575a63d11c0074704
Vernitrax <ty21@op.pl>
2018-02-19 10:22:33 +0100

    prepareMenu method created in view

--- src/view/View.java
+++ src/view/View.java
@@ -1,1 +1,19 @@
 package library;
+
+public class View {
+
+    private Map<Integer, String> menu = new HashMap<>();
+
+    private void prepareMenu() {
+    
+        menu.put(1, "Add new book.");
+        menu.put(2, "Edit book.");
+        menu.put(3, "Delete book.");
+        menu.put(4, "Search for a book.");
+        menu.put(5, "See all books.");
+        menu.put(6, "See all books of given author.");
+        menu.put(0, "Exit.");
+    
+    }
+
+}
