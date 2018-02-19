2a488d5bc37b3ab6af1694ed14e3d6424941eb7b
Vernitrax <ty21@op.pl>
2018-02-19 10:02:25 +0100

    creating database added

---
+++ CreateTables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE books (
+  ISBN INTEGER PRIMARY KEY,
+  FOREIGN KEY (author) REFERENCES authors(author_id) ON DELETE CASCADE,
+  title TEXT NOT NULL,
+  FOREIGN KEY (publisher) REFERENCES publishers(publisher_id) ON DELETE CASCADE,
+  publication_year INTEGER NOT NULL,
+  price INTEGER NOT NULL,
+  FOREIGN KEY (type) REFERENCES type_book(type_id) ON DELETE CASCADE
+  );
+
+CREATE TABLE publishers (
+  publisher_id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+  );
+
+CREATE TABLE type_book (
+  type_id INTEGER PRIMARY KEY,
+  type TEXT NOT NULL
+  );
+
+CREATE TABLE authors (
+  author_id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL,
+  surname TEXT NOT NULL,
+  birth_year INTEGER NOT NULL,
+  city TEXT NOT NULL,
+  country TEXT NOT NULL
+  );
+
